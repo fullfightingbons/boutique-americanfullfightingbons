dbc068517303e7a077e3b43b9d22d67f5310c13a
fullfightingbons <fullfightingbons@gmail.com>
2026-05-17 22:14:41 +0200

    Update index.ts

--- apps/worker-api/src/index.ts
+++ apps/worker-api/src/index.ts
@@ -1,14 +1,22 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+
 import products from './routes/products'
-import admin from './routes/admin'
 import checkout from './routes/checkout'
 import webhook from './routes/webhook'
+import auth from './routes/auth'
+import admin from './routes/admin'
+import upload from './routes/upload'
 
 const app = new Hono()
 
+app.use('*', cors())
+
 app.route('/api', products)
-app.route('/api', admin)
 app.route('/api', checkout)
 app.route('/api', webhook)
+app.route('/api', auth)
+app.route('/api', admin)
+app.route('/api', upload)
 
 export default app
