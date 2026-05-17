5eeb2d3bd818322ea9a8caa68c415972d0ce2068
fullfightingbons <fullfightingbons@gmail.com>
2026-05-17 22:11:44 +0200

    Update App.tsx

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -2,22 +2,22 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Shop from './pages/Shop'
 import Product from './pages/Product'
-import Success from './pages/Success'
 import Admin from './pages/Admin'
+import Success from './pages/Success'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
 export default function App() {
   return (
-    <div className="bg-black text-white min-h-screen">
+    <div className="bg-black text-white min-h-screen overflow-hidden">
       <Navbar />
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/product/:slug" element={<Product />} />
-        <Route path="/success" element={<Success />} />
         <Route path="/admin" element={<Admin />} />
+        <Route path="/success" element={<Success />} />
       </Routes>
 
       <Footer />
