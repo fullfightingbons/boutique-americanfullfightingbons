0b90bf128a7ddffa04483c51691b7f7529fbc90c
fullfightingbons <fullfightingbons@gmail.com>
2026-05-18 11:56:52 +0200

    Delete apps directory

--- apps/frontend/src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="border-t border-zinc-800 p-6 text-center text-zinc-400 mt-10">
-      American Full Fighting Bons © 2026
-    </footer>
-  )
-}
